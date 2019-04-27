7ad5396b17bfda23229e4acf6c35cc3ecafd7c0e
Bernie Pope <bjpope@unimelb.edu.au>
2019-04-27 15:58:49 +1000

    Compute protein z-scores on a per-cancer basis

--- sample_mmr_proteins.py
+++ sample_mmr_proteins.py
@@ -1,6 +1,10 @@
 '''
 Compute the MSH2 and MSH6 protein levels for each sample.
 Only check for COAD, PRAD and UCEC cancer types
+
+We analyse each cancer type separately, so as to compute
+a z-score for that cancer type, rather than all the 3 cancers
+bundled together.
 '''
 
 import csv
@@ -12,15 +16,22 @@ protein_filename = "data/TCGA-PANCAN32-L4.csv"
 
 reader = csv.DictReader(open(protein_filename))
 
-
-msh2_levels = []
-msh6_levels = []
-sample_levels = [] 
+cancer_rows = {'COAD': [], 'UCEC': [], 'PRAD': []}
 
 for row in reader:
-    sample = row['Sample_ID'][:12]
     cancer = row['Cancer_Type']
     if cancer in cancer_types:
+        cancer_rows[cancer].append(row)
+
+print("sample,MSH2_protein,MSH2_protein_z,MSH6_protein,MSH6_protein_z")
+
+for cancer in cancer_rows:
+    msh2_levels = []
+    msh6_levels = []
+    sample_levels = [] 
+    rows = cancer_rows[cancer]
+    for row in rows:
+        sample = row['Sample_ID'][:12]
         # skip samples that don't have recoreded MSH2 or MSH6 levels
         try:
             msh2 = float(row['MSH2'])
@@ -31,13 +42,11 @@ for row in reader:
         except:
             pass
 
-msh2_z_scores = scipy.stats.zscore(msh2_levels)
-msh6_z_scores = scipy.stats.zscore(msh6_levels)
+    msh2_z_scores = scipy.stats.zscore(msh2_levels)
+    msh6_z_scores = scipy.stats.zscore(msh6_levels)
 
-print("sample,MSH2_protein,MSH2_protein_z,MSH6_protein,MSH6_protein_z")
-
-for index in range(len(sample_levels)):
-    sample, msh2, msh6 = sample_levels[index]
-    msh2_z = msh2_z_scores[index]
-    msh6_z = msh6_z_scores[index]
-    print("{},{},{},{},{}".format(sample, msh2, msh2_z, msh6, msh6_z))
+    for index in range(len(sample_levels)):
+        sample, msh2, msh6 = sample_levels[index]
+        msh2_z = msh2_z_scores[index]
+        msh6_z = msh6_z_scores[index]
+        print("{},{},{},{},{}".format(sample, msh2, msh2_z, msh6, msh6_z))
