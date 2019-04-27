292df106768ddcb323f07e13d0fa561dde9c4431
Bernie Pope <bjpope@unimelb.edu.au>
2019-04-27 15:33:27 +1000

    Protein levels for each sample

--- pipeline.sh
+++ pipeline.sh
@@ -6,5 +6,8 @@
 #echo "sample_signatures"
 #python3 sample_signatures.py > data/sample_signatures.csv
 
-echo "sample_mmr_signature"
-python3 sample_mmr_signature.py > data/sample_mmr_signature.csv
+#echo "sample_mmr_signature"
+#python3 sample_mmr_signature.py > data/sample_mmr_signature.csv
+
+echo "sample_mmr_proteins"
+python3 sample_mmr_proteins.py > data/sample_mmr_proteins.csv
---
+++ sample_mmr_proteins.py
@@ -0,0 +1,43 @@
+'''
+Compute the MSH2 and MSH6 protein levels for each sample.
+Only check for COAD, PRAD and UCEC cancer types
+'''
+
+import csv
+import sys
+import scipy.stats
+
+cancer_types = ['COAD', 'PRAD', 'UCEC']
+protein_filename = "data/TCGA-PANCAN32-L4.csv"
+
+reader = csv.DictReader(open(protein_filename))
+
+
+msh2_levels = []
+msh6_levels = []
+sample_levels = [] 
+
+for row in reader:
+    sample = row['Sample_ID'][:12]
+    cancer = row['Cancer_Type']
+    if cancer in cancer_types:
+        # skip samples that don't have recoreded MSH2 or MSH6 levels
+        try:
+            msh2 = float(row['MSH2'])
+            msh6 = float(row['MSH6'])
+            sample_levels.append((sample, msh2, msh6))
+            msh2_levels.append(msh2)
+            msh6_levels.append(msh6)
+        except:
+            pass
+
+msh2_z_scores = scipy.stats.zscore(msh2_levels)
+msh6_z_scores = scipy.stats.zscore(msh6_levels)
+
+print("sample,MSH2_protein,MSH2_protein_z,MSH6_protein,MSH6_protein_z")
+
+for index in range(len(sample_levels)):
+    sample, msh2, msh6 = sample_levels[index]
+    msh2_z = msh2_z_scores[index]
+    msh6_z = msh6_z_scores[index]
+    print("{},{},{},{},{}".format(sample, msh2, msh2_z, msh6, msh6_z))
