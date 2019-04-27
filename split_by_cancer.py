980c19d012d385ede5a3ce14c14035c0d08badca
Bernie Pope <bjpope@unimelb.edu.au>
2019-04-27 18:25:46 +1000

    split results into cancer type

---
+++ split_by_cancer.py
@@ -0,0 +1,28 @@
+'''
+Split the joined file by cancer type
+'''
+
+import sys
+import csv
+import os.path
+
+cancer_types = ['PRAD', 'UCEC', 'COAD']
+
+joined_filename = "data/join_samples.csv"
+
+reader = csv.reader(open(joined_filename))
+header = next(reader)
+
+cancer_data = {'PRAD': [], 'UCEC': [], 'COAD': []}
+
+for row in reader:
+    cancer = row[1]
+    cancer_data[cancer].append(row)
+
+for cancer in cancer_data:
+    output_filename = os.path.join('data', cancer + '.join_samples.csv')
+    output_file = open(output_filename, "w")
+    print(",".join(header), file=output_file)
+    for row in cancer_data[cancer]:
+        print(",".join(row), file=output_file)
+    output_file.close()
