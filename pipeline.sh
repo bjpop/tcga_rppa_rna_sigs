10dab56166a52edc4f3572f28b85affdaff21e62
Bernie Pope <bjpope@unimelb.edu.au>
2019-04-27 15:12:46 +1000

    Add stage

--- pipeline.sh
+++ pipeline.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "sample_cancer"
-python3 sample_cancer.py > sample_cancer.csv
+# echo "sample_cancer"
+# python3 sample_cancer.py > data/sample_cancer.csv
+
+#echo "sample_signatures"
+#python3 sample_signatures.py > data/sample_signatures.csv
+
+echo "sample_mmr_signature"
+python3 sample_mmr_signature.py > data/sample_mmr_signature.csv
